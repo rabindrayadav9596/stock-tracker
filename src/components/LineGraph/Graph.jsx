95e31ee42244976159334ade4d7de18529c91161
Rabindra Yadav <rabindrayadav9596@gmail.com>
2020-06-03 14:13:52 -0500

    fixed fetching open data

--- src/components/LineGraph/Graph.jsx
+++ src/components/LineGraph/Graph.jsx
@@ -1,26 +1,27 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Line } from "react-chartjs-2";
 import styles from "./Graph.module.css";
 const Graph = ({ data }) => {
   console.log(data);
   const stockData = Object.values(data);
   console.log(stockData);
-  //   console.log(stockData);
-  //   const todayStock = stockData[0];
-  //   console.log(todayStock);
-  //   //console.log(todayStock["1. open"]);
-  //   //console.log(Object.keys(data));
-  //   const openValue = todayStock.map((value) => {
-  //     value["1. open"];
-  //   });
 
-  const lineChart = (
+  const todayStock = stockData[0];
+  console.log(todayStock);
+  let todayStockValue = null;
+  if (todayStock) {
+    console.log(todayStock);
+    todayStockValue = Object.values(todayStock);
+    console.log(todayStockValue);
+  }
+
+  const lineChart = todayStock ? (
     <Line
       data={{
         labels: Object.keys(data),
         datasets: [
           {
-            data: [4, 5, 6, 7, 9, 11],
+            data: todayStockValue,
 
             label: "Open",
             borderColor: "#3333ff",
@@ -29,7 +30,7 @@ const Graph = ({ data }) => {
         ],
       }}
     />
-  );
+  ) : null;
 
   //   const lineChart = data[0] ? (
   //     <Line
